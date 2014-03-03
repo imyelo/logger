2afef9e5144e4f7523a9a55c22140ad490f53b5e
yelo <zhihuzeye@gmail.com>
2014-03-03 09:45:53 +0800

    update test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,10 @@ var chai = require('chai');
 var muk = require('muk');
 var expect = chai.expect;
 var fs = require('fs');
+var path = require('path');
 var mkdirp = require('mkdirp');
 var log = require('../');
+var utils = require('../lib/utils');
 
 describe('base', function () {
   describe('case', function () {
@@ -12,7 +14,7 @@ describe('base', function () {
       muk(fs, 'createWriteStream', function (path) {
         return {
           write: function (content) {
-            console.log('abc');
+            console.log('muk');
             return {
               content: content,
               path: path
@@ -20,13 +22,31 @@ describe('base', function () {
           }
         };
       });
+      muk(utils, 'getIP', function () {
+        return '192.168.999.999';
+      });
+      muk(Date, 'now', function () {
+        return 1393810654681;
+      });
     });
     after(function () {
       muk.restore();
     });
     it('1', function () {
       var Logger = log.Logger;
-      expect(Logger().to(25).interface('/interface').done()).to.be.equal('');
+      var dir = path.join(__dirname, '../lib/log/production.log');
+      var content = {
+        "@timestamp":"2014-03-03T09:37:34+08:00",
+        "@source": "192.168.999.999",
+        "@fields": {
+          "fromtype": "myApp",
+          "totype": 25,
+          "interface": "/interface"
+        },
+      };
+      var result = Logger().to(25).interface('/interface').done();
+      expect(result.path).to.be.deep.equal(dir);
+      expect(JSON.parse(result.content)).to.be.deep.equal(content);
     });
   });
 });
